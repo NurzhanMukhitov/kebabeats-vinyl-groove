92720cb3d97f7578d73a9b5a178072daaf9281bc
NurzhanMukhitov <n.mukhitov@larnilane.com>
2026-03-18 17:17:30 +0100

    Add new track 'KazzJazz HipHop' to tracks data

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -23,6 +23,17 @@ export const TRACKS: Track[] = [
     audioUrl: 'https://pub-ffa331fb4e5045e2a665d7fdeb5fbcda.r2.dev/shimoon_mp3.mp3',
     coverUrl: '',
   },
+  {
+    id: '3',
+    title: 'KazzJazz HipHop',
+    artist: 'KEBABEATS',
+    venue: '',
+    date: '',
+    duration: '',
+    durationSeconds: 0,
+    audioUrl: 'https://pub-ffa331fb4e5045e2a665d7fdeb5fbcda.r2.dev/KazzJazz-HipHop.mp3',
+    coverUrl: '',
+  },
 ];
 
 export const SLIPMAT_IMAGE = '/mascot.png';
